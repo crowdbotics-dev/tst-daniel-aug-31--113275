73d1005ab9d1e29b122477032a8b4c77aed00432
Daniel Sousa <daniel.s@crowdbotics.com>
2023-09-01 21:06:08 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ modules/basiq-payments/options.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  heading: {
+    height: 60,
+    backgroundColor: "#333333",
+    padding: 20,
+    alignItems: "flex-end",
+    flexDirection: "row",
+    justifyContent: "center"
+  }
+});
+export default {
+  styles: styles
+};
